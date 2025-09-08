37afd78bf985aa4655e642d144b7110895bde12d
RadMann <rado.harvester@gmail.com>
2025-09-08 18:35:52 +0200

    init v2

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::get('/contact', [ContactController::class, 'index']);
